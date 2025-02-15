fc90541577a44051b07056ad8207f6c1ee90bbfb
satria_dicoding <nugrahasatria404@gmail.com>
2025-02-15 17:53:08 +0800

    fix

--- main.tf
+++ main.tf
@@ -87,51 +87,6 @@ resource "aws_instance" "dicoding_terraform" {
     volume_size = 15
   }
 
-  user_data = <<-EOF
-                #!/bin/bash
-                apt update -y
-                apt install -y ca-certificates curl docker-ce docker-ce-cli containerd.io docker-buildx-plugin docker-compose-plugin
-                systemctl enable docker
-                systemctl start docker
-                usermod -aG docker ubuntu
-                
-                # Buat network Jenkins
-                docker network create jenkins
-                
-                # Buat Dockerfile untuk Jenkins
-                cat <<EOD > Dockerfile
-                FROM jenkins/jenkins:2.426.2-jdk17
-                USER root
-                RUN apt-get update && apt-get install -y lsb-release
-                RUN curl -fsSLo /usr/share/keyrings/docker-archive-keyring.asc \\
-                    https://download.docker.com/linux/debian/gpg
-                RUN echo "deb [arch=\$(dpkg --print-architecture) \\
-                    signed-by=/usr/share/keyrings/docker-archive-keyring.asc] \\
-                    https://download.docker.com/linux/debian \\
-                    \$(lsb_release -cs) stable" > /etc/apt/sources.list.d/docker.list
-                RUN apt-get update && apt-get install -y docker-ce-cli
-                USER jenkins
-                RUN jenkins-plugin-cli --plugins "blueocean:1.27.9 docker-workflow:572.v950f58993843"
-                EOD
-
-                # Build image Jenkins
-                docker build -t myjenkins-blueocean:2.426.2-1 .
-
-                # Jalankan Jenkins
-                docker run --name jenkins-blueocean --detach --network jenkins --env DOCKER_HOST=tcp://docker:2376 --env DOCKER_CERT_PATH=/certs/client --env DOCKER_TLS_VERIFY=1 --publish 8080:8080 --publish 50000:50000 \
-                    --volume jenkins-data:/var/jenkins_home --volume jenkins-docker-certs:/certs/client:ro --restart=on-failure --env JAVA_OPTS="-Dhudson.plugins.git.GitSCM.ALLOW_LOCAL_CHECKOUT=true" myjenkins-blueocean:2.426.2-1 
-
-                # Jalankan Docker dind
-                docker run --name jenkins-docker --detach --privileged --network jenkins --network-alias docker --env DOCKER_TLS_CERTDIR=/certs --volume jenkins-docker-certs:/certs/client \
-                    --volume jenkins-data:/var/jenkins_home --publish 2376:2376 --restart always docker:dind --storage-driver overlay2
-                
-                # Jalankan Prometheus
-                docker run -d -p 9091:9090 --name prometheus --restart=always prom/prometheus
-
-                # Jalankan Grafana
-                docker run -d --name grafana -p 3031:3031 -e "GF_SERVER_HTTP_PORT=3031" grafana/grafana
-              EOF
-
   tags = {
     Name = "Dicoding-Terraform"
   }
